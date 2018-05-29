1408e3bac538a78e592674d88c724c9cc3fc9217
brandonvdongen <brandonvdongen@hotmail.com>
2018-05-29 11:58:00 +0200

    fixed up ccs, added server/app.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
 import App from './App.html';
 
 const app = new App({
-	target: document.body,
-	data: {
-		name: 'Brandon'
-	}
+	target: document.body
 });
-
 window.app = app;
-
 export default app;
