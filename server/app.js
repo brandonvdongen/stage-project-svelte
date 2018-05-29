cc3ea580c375c9e4ce5d184b3aeac1ac1bd8e034
brandonvdongen <brandonvdongen@hotmail.com>
2018-05-29 17:01:22 +0200

    moved fetching to serverside

--- server/app.js
+++ server/app.js
@@ -1,6 +1,28 @@
-var http = require('http');
+//todo express vervangen met eigen http/routing
+const express = require('express');
+const fetch = require('node-fetch'); //node fetch voor de consistency met de clientside javascript
+const app = express();
 
-http.createServer(function (req, res) {
-    res.write('Hello World!'); //write a response to the client
-    res.end(); //end the response
-}).listen(8080);
+/***
+ * search route, formatted as localhost/search/[tags]/[page number]
+ */
+app.get(['/search/:search/', '/search/:search/:page'], (req, res) => {
+    console.log(req.params);
+    const page = req.params.page || 1;
+    const per_page = 50;
+    const url = "https://pixabay.com/api/?key=8990826-747bd9300ee0354a8b5e85d26&q=" + encodeURI(req.params.search) + "&image_type=photo&per_page=" + per_page + "&page=" + page + "&orientation=horizontal";
+    fetch(url).then(value => {
+        return value.json();
+    }).then(value => {
+        value["status"]="success";
+        console.log(value);
+        res.send(value);
+    }).catch(reason=>{
+        reason["status"]="error";
+        res.send(reason);
+    });
+});
+
+app.use(express.static('../public'));
+
+app.listen(8888, () => console.log('Example app listening on port 8888!'));
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import App from './App.html';
 
 const app = new App({
-	target: document.body
+	target: document.body,
+	data:{
+		server:"http://localhost:8888"
+	}
 });
 window.app = app;
 export default app;
